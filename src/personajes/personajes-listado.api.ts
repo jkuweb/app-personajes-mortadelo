97a5e6df74a9006f4b743a3619c731cfc3a47664
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-07-06 18:46:44 +0200

    llamada a la API con el parámetro de busqueda por nombre de personaje

--- src/personajes/personajes-listado.api.ts
+++ src/personajes/personajes-listado.api.ts
@@ -5,19 +5,27 @@ export const API_URL = "http://localhost:3000";
 
 export const obtenerPersonajes = async (): Promise<Personaje[]> => {
   try {
-    const personajes = await axios.get(`${API_URL}/personajes`)
-    return personajes.data
+    const personajes = await axios.get(`${API_URL}/personajes`);
+    return personajes.data;
   } catch (error) {
-    throw new Error("No se ha podido obtener los personajes")
+    throw new Error("No se ha podido obtener los personajes");
   }
 }
 
 export const obtenerImagenDelPersonaje = async (nombreDelPersonaje: string): Promise<string> => {
   try {
     const imagenDelPersonaje = await axios(`${API_URL}/${nombreDelPersonaje}`)
-    console.log(imagenDelPersonaje)
-    return imagenDelPersonaje.data
+    return imagenDelPersonaje.data;
   } catch (error) {
-    throw new Error("No se ha podido obtener las imágenes de los personajes")
+    throw new Error("No se ha podido obtener las imágenes de los personajes");
+  }
+}
+
+export const obtenerPersonaFiltrada = async (nombrePersonaje: string): Promise<Personaje> => {
+  try {
+    const personaje = await axios.get(`http://localhost:3000/personajes?nombre_like=${nombrePersonaje}`);
+    return personaje.data;
+  } catch (error) {
+    throw new Error("No e ha podido obtener el personaje filtrado");
   }
 }
