179e8e3e85251d4620d2406dd18ce05db0c0d807
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-07-07 09:48:37 +0200

    formatear los archivos

--- src/personajes/personajes-listado.api.ts
+++ src/personajes/personajes-listado.api.ts
@@ -10,23 +10,29 @@ export const obtenerPersonajes = async (): Promise<Personaje[]> => {
   } catch (error) {
     throw new Error("No se ha podido obtener los personajes");
   }
-}
+};
 
-export const obtenerImagenDelPersonaje = async (nombreDelPersonaje: string): Promise<string> => {
+export const obtenerImagenDelPersonaje = async (
+  nombreDelPersonaje: string
+): Promise<string> => {
   try {
-    const imagenDelPersonaje = await axios(`${API_URL}/${nombreDelPersonaje}`)
+    const imagenDelPersonaje = await axios(`${API_URL}/${nombreDelPersonaje}`);
     return imagenDelPersonaje.data;
   } catch (error) {
     throw new Error("No se ha podido obtener las imágenes de los personajes");
   }
-}
+};
 
-export const obtenerPersonajesFiltrado = async (nombrePersonaje: string): Promise<Personaje[]> => {
+export const obtenerPersonajesFiltrado = async (
+  nombrePersonaje: string
+): Promise<Personaje[]> => {
   try {
-    const personajes = await axios.get(`http://localhost:3000/personajes?nombre_like=${nombrePersonaje}`);
-    console.log(personajes.data)
+    const personajes = await axios.get(
+      `http://localhost:3000/personajes?nombre_like=${nombrePersonaje}`
+    );
+    console.log(personajes.data);
     return personajes.data;
   } catch (error) {
     throw new Error("No e ha podido obtener el personaje filtrado");
   }
-}
+};
--- src/personajes/personajes-listado.model.ts
+++ src/personajes/personajes-listado.model.ts
@@ -1,9 +1,9 @@
 export interface Personaje {
-  id: string,
-  nombre: string,
-  apodo: string,
-  especialidad: string,
-  habilidades: string[],
-  amigo: string,
-  imagen: string
-} 
+  id: string;
+  nombre: string;
+  apodo: string;
+  especialidad: string;
+  habilidades: string[];
+  amigo: string;
+  imagen: string;
+}
